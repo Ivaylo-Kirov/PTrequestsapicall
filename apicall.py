8873128e6d3b43e73dabe497857e2fe6ad5fe485
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-09-10 13:58:29 -0400

    Round 2 - added call to API Gateway

--- apicall.py
+++ apicall.py
@@ -1,6 +1,7 @@
 import requests
 
-r = requests.get('https://jsonplaceholder.typicode.com/posts')
+#r = requests.get('https://jsonplaceholder.typicode.com/posts')
+r = requests.get('https://hlv7793ve8.execute-api.us-east-1.amazonaws.com/jsontest')
 
 print(r.status_code)
 print("---")
